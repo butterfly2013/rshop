83dac9d70b47316a934e41b6b9291f8c7cfde721
butterfly2013 <lclllccll@163.com>
2013-10-20 09:30:28 +0800

    提交项目

---
+++ protected/config/constants.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * 系统常量定义
+ */
+
+define('SITE_URL', 'www.bf.com');
+//设置前台资源
+define('CSS_URL',SITE_URL.'assets/default/css');
+define('IMG_URL', SITE_URL.'assets/default/img');
+//设置后台资源
+define('CSS_URL',SITE_URL.'assets/houtai/css');
+define('IMG_URL', SITE_URL.'assets/houtai/img');
+?>
